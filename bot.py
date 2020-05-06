288130d7fcee1af31f2f555bbd016f9a4f28861a
Александр <a.sedov@a1qa.com>
2020-05-07 02:10:14 +0300

    commit for test

--- bot.py
+++ bot.py
@@ -66,6 +66,7 @@ if __name__ == '__main__':
         fallbacks=[CommandHandler('reset', start, pass_user_data=True)]
     )
 
+    dp.add_handler(conv_handler)
     # log all errors
     dp.add_error_handler(error)
     run(updater)
