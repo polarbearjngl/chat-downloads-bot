d3f65712945f66a2f168b8f6eee2e984805caca4
Александр <a.sedov@a1qa.com>
2020-05-12 00:33:12 +0300

    commit for test

--- bot/handlers.py
+++ bot/handlers.py
@@ -1,6 +1,8 @@
 from datetime import datetime
 from functools import wraps
 
+import pytz
+
 from bot.common import GET_DOCUMENT, DOWNLOAD_FILE, TARGET_CHAT, LIST_OF_ADMINS, DATABASE_URL
 from menu import Menu, MenuList
 from telegram import InlineKeyboardMarkup
@@ -74,8 +76,6 @@ def call_handler(bot, update):
                             last_name=from_user.last_name,
                             username=from_user.username,
                             is_bot=from_user.is_bot,
-                            download_date=datetime.now(),
-                            filename="test")
+                            download_date=datetime.now(tz=pytz.timezone('Europe/Moscow')),
+                            filename=re.findall(r'Скачать файл: (.*\n+)', query.message.to_json()['text'])[0])
         downloads_db.close()
-
-        from_user.send_message(text=query.message.to_json())
